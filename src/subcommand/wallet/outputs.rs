dcb734a6b040132cfc356781d25a95e30243555a
Danny Diekroeger <dannydiek@gmail.com>
2023-02-03 17:14:24 -0800

    dont use index in outputs

--- src/subcommand/wallet/outputs.rs
+++ src/subcommand/wallet/outputs.rs
@@ -7,11 +7,25 @@ pub struct Output {
 }
 
 pub(crate) fn run(options: Options) -> Result {
-  let index = Index::open(&options)?;
-  index.update()?;
+  // let index = Index::open(&options)?;
+  // index.update()?;
+  let client = options.bitcoin_rpc_client_for_wallet_command(false)?;
+
+  let mut utxos = BTreeMap::new();
+  utxos.extend(
+      client
+      .list_unspent(None, None, None, None, None)?
+      .into_iter()
+      .map(|utxo| {
+        let outpoint = OutPoint::new(utxo.txid, utxo.vout);
+        let amount = utxo.amount;
+
+        (outpoint, amount)
+      }),
+  );
 
   let mut outputs = Vec::new();
-  for (output, amount) in index.get_unspent_outputs()? {
+  for (output, amount) in utxos {
     outputs.push(Output {
       output,
       amount: amount.to_sat(),
