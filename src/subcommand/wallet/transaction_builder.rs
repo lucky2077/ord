5efc1d9a76a98905b07acc0d4e3695bdc18c1fb5
Danny Diekroeger <dannydiek@gmail.com>
2023-04-12 10:36:15 -0700

    log min value

--- src/subcommand/wallet/transaction_builder.rs
+++ src/subcommand/wallet/transaction_builder.rs
@@ -635,7 +635,7 @@ impl TransactionBuilder {
       .map(|satpoint| satpoint.outpoint)
       .collect::<BTreeSet<OutPoint>>();
 
-    print_json(minimum_value);
+    println!(minimum_value);
 
     for utxo in &self.utxos {
       if inscribed_utxos.contains(utxo) {
